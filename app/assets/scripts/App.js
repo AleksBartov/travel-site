6bb4aca1997e0bd04db08e14889ede9293d39f3b
Aleksandr <aleksandrbartov@yandex.ru>
2018-03-20 03:03:30 +0300

    all is looking well

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,3 @@
-var $ = require('jquery');
-var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-alert('testing 12345678910');
-
-var john = new Person("Sasha Bartov", "red");
-john.greet();
-
-$('h1').hide();
+var mobileMenu = new MobileMenu();
--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(fullName, favColor){
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function(){
-    console.log("hello, my name is " + this.name);
-  }
-}
-
-module.exports = Person;
