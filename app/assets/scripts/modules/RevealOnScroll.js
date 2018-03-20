d813e756a9562276fb012d17cf20171f072427a0
Aleksandr <aleksandrbartov@yandex.ru>
2018-03-20 03:51:56 +0300

    i have learnd to reveal any element of my site

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $('.feature-item');
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      var that = this;
+      new Waypoint({
+        element: that,
+        handler: function() {
+          $(that).addClass('reveal-item--is-visible');
+        },
+        offset: '85%'
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
