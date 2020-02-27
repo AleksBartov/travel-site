a8aed23de638a3d0701392c1a448b60f7abe29dd
Aleksandr <aleksandrbartov@yandex.ru>
2020-02-27 05:48:29 +0300

    change travel on fifteen

---
+++ docs/precache-manifest.46e56c335c092ec1a0542db6899b4819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46805b4aae5ec35a704d2d0ba1f33474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bbca3dbf97b1909d0c",
+    "url": "/static/css/main.251bd3d3.chunk.css"
+  },
+  {
+    "revision": "b50e46802fde422c0707",
+    "url": "/static/js/2.b8a1921d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b8a1921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bbca3dbf97b1909d0c",
+    "url": "/static/js/main.bc5766b4.chunk.js"
+  },
+  {
+    "revision": "3a4adb320cdcc071cc8b",
+    "url": "/static/js/runtime-main.0a939b85.js"
+  }
+]);
